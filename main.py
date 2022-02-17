1cfc7aac9c2a3d543d92159bb3c70b619c7fb129
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-02-17 15:58:38 +0800

    Replaced add command with botinfo command.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import disnake
 from disnake.ext import commands 
 
+intents = disnake.Intents.default()
+color = 11393254
 guilds = []
 
 if len(guilds) == 0:
@@ -9,7 +11,9 @@ if len(guilds) == 0:
 
 ark = commands.Bot(
     description="A discord bot.",
-    test_guilds=guilds
+    test_guilds=guilds,
+    sync_commands = True,
+    intents=intents 
 )
 
 @ark.event
@@ -24,14 +28,21 @@ async def on_ready():
 async def ping(inter):
     embed = disnake.Embed(
         title="Pong!", 
-        color=disnake.Colour(value=11393254)
+        color=disnake.Colour(value=color)
     )
     embed.add_field(name="Latency:", value=f"`{ark.latency*1000:.2f}ms`")
     embed.set_thumbnail("https://c.tenor.com/LqNPvLVdzHoAAAAC/cat-ping.gif")
     await inter.response.send_message(embed=embed)
 
-@ark.slash_command(description="Adds two numbers.")
-async def add(inter, addend1: int, addend2: int):
-    await inter.response.send_message(addend1 + addend2)
+@ark.slash_command(description="Sends info about the bot.")
+async def botinfo(inter):
+    info = await ark.application_info()
+    description = f"Owner: {info.owner}"
+    embed = disnake.Embed(
+        title="Some info about me:",
+        description=description,
+        color=disnake.Colour(value=color)
+    )
+    await inter.response.send_message(embed=embed)
 
 ark.run(os.getenv("TOKEN"))
