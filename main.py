1e5651775794e6a39599d6e5b3af454a4136bfbe
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-02-18 07:42:45 +0800

    Improved botinfo command

--- main.py
+++ main.py
@@ -33,18 +33,24 @@ async def ping(inter):
     )
     embed.add_field(name="Latency:", value=f"`{ark.latency*1000:.2f}ms`")
     embed.set_thumbnail("https://c.tenor.com/LqNPvLVdzHoAAAAC/cat-ping.gif")
+
     await inter.response.send_message(embed=embed)
 
 @ark.slash_command(description="Sends info about the bot.")
 async def botinfo(inter):
     info = await ark.application_info()
+    bot_user = ark.user
     embed = disnake.Embed(
         title="Some info about me:",
         color=disnake.Colour(value=color)
     )
     embed.add_field(name="Owner:", value=info.owner)
-    embed.add_field(name="Bot ID:", value=ark.user.id)
+    embed.add_field(name="Bot ID:", value=bot_user.id)
+    embed.add_field(name="Created at:", value=disnake.utils.format_dt(bot_user.created_at, style="F"))
+    embed.add_field(name="Servers I'm in:", value=len(ark.guilds))
+    embed.add_field(name="Current number of commands:", value=len(ark.slash_commands))
     embed.set_thumbnail(ark.user.avatar)
+
     await inter.response.send_message(embed=embed)
 
 ark.run(os.getenv("TOKEN"))
